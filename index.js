fa8a63dd23195868e536f9a4de45d48e9c0cac54
caseytansey <ctansey12600@outlook.com>
2021-08-28 13:32:27 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName,userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    })
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(Obj){
+        let id = Obj.id
+        document.querySelector('body').append(id)
+    })
+    .catch(function(Obj){
+        let value = Obj.message
+        document.querySelector('body').append(value)
+    })
+}
